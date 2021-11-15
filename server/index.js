c00d9543526185b34b4b8430f85c2f634db7c21d
Dakota Malchow <dakotamalchow@gmail.com>
2021-11-14 19:56:21 -0800

    Move get to correct position

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req,res) => {
+    res.send("Hello, world");
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-
-app.get("/", (req,res) => {
-    res.send("Hello, world");
-})
