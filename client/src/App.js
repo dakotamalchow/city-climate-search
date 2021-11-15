e32cfdf96c0ceb9f7ec8de84b86d30809bffbae6
Dakota Malchow <dakotamalchow@gmail.com>
2021-11-14 19:36:26 -0800

    Move react code to client folder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Results from './Results';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>City Climate Search</h1>
+        <Results />
+      </div>
+    );
+  }
+}
+
+export default App;
