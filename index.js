082caab5c5c86ae9f79a2095fc932d8421cdf16a
Dakota Malchow <dakotamalchow@gmail.com>
2021-11-25 15:07:46 -0800

    Add packages for web scraping

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const request = require("request");
+const cheerio = require("cheerio");
 
 app.get("/", (req,res) => {
     res.set("Access-Control-Allow-Origin","*");
