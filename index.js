87f4f85812143f1b433c5da222e6524a4eeab7b4
Dakota Malchow <dakotamalchow@gmail.com>
2021-11-26 23:06:40 -0800

    Get links to top cities

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const request = require("request");
+const axios = require("axios");
 const cheerio = require("cheerio");
 
-app.get("/", (req,res) => {
+app.get("/", async (req,res) => {
     res.set("Access-Control-Allow-Origin","*");
-    res.send({message: "Backend is connected to React"});
+
+    const extractLinks = $ => {
+        return $("tr:first-child a")
+            .filter((_, element) => {
+                return $(element).attr("title")
+                && !$(element).attr("href").includes("Template")
+                && !$(element).attr("href").includes("United_States")
+            })
+            .map((_, element) => $(element).attr("href"))
+            .toArray()
+    };
+
+    const cityLinks = await axios.get("https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population")
+        .then(({data}) => {
+            const $ = cheerio.load(data);
+            return extractLinks($);
+        });
+    res.send({message: cityLinks.join("\r\n")});
 });
 
 app.listen(port, () => {
