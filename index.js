e4d02a17013bdb28fbcc502bf013de3a987ae5b9
Dakota Malchow <dakotamalchow@gmail.com>
2021-11-27 18:14:29 -0800

    Get climate table

--- index.js
+++ index.js
@@ -7,23 +7,37 @@ const cheerio = require("cheerio");
 app.get("/", async (req,res) => {
     res.set("Access-Control-Allow-Origin","*");
 
-    const extractLinks = $ => {
+    const extractCityLinks = $ => {
         return $("tr:first-child a")
-            .filter((_, element) => {
-                return $(element).attr("title")
-                && !$(element).attr("href").includes("Template")
-                && !$(element).attr("href").includes("United_States")
+            .filter((_, el) => {
+                return $(el).attr("title")
+                && !$(el).attr("href").includes("Template")
+                && !$(el).attr("href").includes("United_States");
             })
-            .map((_, element) => $(element).attr("href"))
-            .toArray()
+            .map((_, el) => $(el).attr("href"))
+            .toArray();
     };
 
+    const extractClimateData = $ => {
+        return $("tr th")
+            .filter((_, el) => $(el).text().includes("Climate"))
+            .map((_, el) => $(el).text())
+            .toArray();
+    };
+
+    // is currently only returning the first 100 results
     const cityLinks = await axios.get("https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population")
         .then(({data}) => {
             const $ = cheerio.load(data);
-            return extractLinks($);
+            return extractCityLinks($);
         });
-    res.send({message: cityLinks.join("\r\n")});
+    
+        const climateData = await axios.get("https://en.wikipedia.org" + cityLinks[1])
+        .then(({data}) => {
+            const $ = cheerio.load(data);
+            return extractClimateData($);
+        });
+    res.send({message: climateData});
 });
 
 app.listen(port, () => {
