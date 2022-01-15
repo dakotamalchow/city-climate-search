0f3cb9f45e283ad3d91c25b9a9469219b593aacd
Dakota Malchow <dakotamalchow@gmail.com>
2022-01-14 21:08:41 -0800

    Add type

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ mongoose.connect("mongodb://127.0.0.1:27017")
     .then(() => {
         console.log("Mongo connection open");
     })
-    .catch(error => {
+    .catch((error: Error) => {
         console.log("Error connecting to Mongo: " + error);
     });
 
